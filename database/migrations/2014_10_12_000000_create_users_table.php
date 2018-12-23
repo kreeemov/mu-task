d7d30bc27f031381efa5ed013fa359c4030d7d58
Abdulkareem Musfer <almusfer.k@gmail.com>
2018-12-23 15:16:38 +0300

    Auth Created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->bigInteger('national_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('gender')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
